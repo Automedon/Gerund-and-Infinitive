ef2f4efae435fb6391e8fee3e0948faa321a6070
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-12 16:27:16 +0700

    Add files via upload

---
+++ docs/precache-manifest.bc1f0f459422ec912159713449db1f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e682c894a2741b5775eccd872cf0940f",
+    "url": "/Gerund-and-Infinitive/index.html"
+  },
+  {
+    "revision": "93c7db192fdc2be86979",
+    "url": "/Gerund-and-Infinitive/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "8723b84b947abfc00982",
+    "url": "/Gerund-and-Infinitive/static/css/main.70256f86.chunk.css"
+  },
+  {
+    "revision": "93c7db192fdc2be86979",
+    "url": "/Gerund-and-Infinitive/static/js/2.37bcfff4.chunk.js"
+  },
+  {
+    "revision": "8723b84b947abfc00982",
+    "url": "/Gerund-and-Infinitive/static/js/main.735039f3.chunk.js"
+  },
+  {
+    "revision": "eac35fbb3b25e5906693",
+    "url": "/Gerund-and-Infinitive/static/js/runtime-main.2013398b.js"
+  }
+]);
